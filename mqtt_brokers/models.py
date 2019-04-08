eb37feba2ff4db31813d06717d59d2597709dd90
LordMascachapas <alfredm3007@gmail.com>
2019-04-08 17:41:30 +0200

    Broker duties in settings

--- mqtt_brokers/models.py
+++ mqtt_brokers/models.py
@@ -1,14 +1,10 @@
 from django.db import models
-
-DUTIES = [
-    ("logs", "doors logs listener"),
-    ("management", "order publisher")
-]
+from door_brain.settings import BROKER_DUTIES
 
 
 class Broker(models.Model):
     ip = models.CharField(max_length=30, primary_key=True, default='X.X.X.X')
-    duty = models.CharField(choices=DUTIES, max_length=50)
+    duty = models.CharField(choices=BROKER_DUTIES.get("choices"), max_length=50)
 
     def __str__(self):
         return self.duty + ' - ' + self.ip
