b363265d6d6cbc7944995f44ca13474edd6ab2a3
LordMascachapas <alfredm3007@gmail.com>
2019-04-08 17:23:01 +0200

    Minor fixes in Log model

--- logs/models.py
+++ logs/models.py
@@ -3,18 +3,18 @@ from doors.models import Door
 
 
 class Log(models.Model):
-    card_authorized = models.BooleanField()
+    card_authorized = models.BooleanField(default=False)
     reason = models.CharField(max_length=1000, default='')
-    date_time = models.DateTimeField()
+    date_time = models.DateTimeField(default=None)
     door = models.ForeignKey(Door, on_delete=models.SET_NULL, null=True)
-    card_hash = models.CharField(max_length=300)
+    card_hash = models.CharField(max_length=300, default=None)
 
     class Meta:
         unique_together = (("date_time", "door"),)
 
     def __str__(self):
         msg = self.date_time.strftime('%m-%d %H:%M')
-        msg += ''
+        msg += ' '
         msg += 'AUTHORIZED ' if self.card_authorized else 'DENIED '
         msg += 'in door ' + self.door.id
         return msg
