94cd41f231453f98447dc7d7b305141ecdcc0f89
LordMascachapas <alfredm3007@gmail.com>
2019-04-08 17:28:57 +0200

    Minor fixes in Card model

--- cards/models.py
+++ cards/models.py
@@ -4,7 +4,7 @@ from custom_users.models import CustomUser
 
 class Card(models.Model):
     hash = models.CharField(primary_key=True, max_length=300, default=None)
-    created_at = models.DateTimeField(auto_now_add=True, default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
