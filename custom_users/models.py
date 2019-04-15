fec346dbc1205aad970166dd64d88d0e30300cae
LordMascachapas <alfredm3007@gmail.com>
2019-04-15 13:07:35 +0200

    Users and groups have a many to many relationship

--- custom_users/models.py
+++ custom_users/models.py
@@ -4,7 +4,7 @@ from mqtt_groups.models import MqttGroup
 
 class CustomUser(models.Model):
     username = models.CharField(primary_key=True, max_length=100, default=None)
-    group = models.ForeignKey(MqttGroup, on_delete=models.SET_NULL, null=True)
+    groups = models.ManyToManyField(MqttGroup)
 
     def __str__(self):
         return self.username
