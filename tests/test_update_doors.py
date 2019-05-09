251fbc8c39514315f3860231728f81672c9f8c0b
LordMascachapas <alfredm3007@gmail.com>
2019-05-09 13:42:39 +0200

    testcase for door update endpoint

--- tests/test_update_doors.py
+++ tests/test_update_doors.py
@@ -1,105 +1,62 @@
 from django.test import client, TestCase
-import datetime
-
-from mqtt_brokers.models import Broker
-from doors.models import Door
-from mqtt_groups.models import MqttGroup
-from mqtt_rules.models import WeekRules
-from custom_users.models import CustomUser
-from cards.models import Card
+from django.urls import reverse
+from tests.data_base_test_utils import setup_db_for_test, security_door_result, storage_door_result, hall_door_result
+import paho.mqtt.client as mqtt
+import time
+import json
 
 C = client.Client()
 
 
 class UpdateDoorsTest(TestCase):
     def setUp(self):
-        curent_rule_hk = WeekRules(
-            indentf="current_rule",
-            initial_hour_access="05:00",
-            duration=datetime.timedelta(hours=5),
-            days_per_week=[1, 5, 7],
-            permission_level=1,
-            initial_date="2019-01-23T00:00:00.000000Z",
-            expiration_date="2020-01-23T00:00:00.000000Z",
-        )
-        curent_rule_s = WeekRules(
-            indentf="current_rule",
-            initial_hour_access="05:00",
-            duration=datetime.timedelta(hours=5),
-            days_per_week=[1, 5, 7],
-            permission_level=1,
-            initial_date="2019-01-23T00:00:00.000000Z",
-            expiration_date="2020-01-23T00:00:00.000000Z",
-        )
-        old_rule = WeekRules(
-            identf="old_rule",
-            initial_hour_access="05:00",
-            duration=datetime.timedelta(hours=5),
-            days_per_week=[1, 5, 7],
-            permission_level=1,
-            initial_date="2014-01-23T00:00:00.000000Z",
-            expiration_date="2025-01-23T00:00:00.000000Z",
-        )
+        setup_db_for_test()
+        self.setup_door_clients()
 
-        housekeeping = MqttGroup(name="housekeeping",)
-        security = MqttGroup(name="security")
-        old_residents = MqttGroup(name="old_residents")
+    def setup_door_clients(self):
+        self.security_door_mqtt_client = mqtt.Client()
+        self.security_door_mqtt_client.connect(host="127.0.0.1", port=1883)
+        self.security_door_mqtt_client.subscribe(topic="sc_2")
+        self.security_door_mqtt_client.on_message = lambda client, userdata, message: self.assertSecurityUsersUpdated(
+            message)
+        self.security_door_mqtt_client.loop_start()
+        self.security_door_sentinel = False
 
-        gilbert = CustomUser(username="Gilbert")
-        anna = CustomUser(username="Anna")
-        maria = CustomUser(username="Maria")
-        marta = CustomUser(username="Marta")
-        mr_misterius = CustomUser(username="Mr_Misterius")
+        self.storage_door_mqtt_client = mqtt.Client()
+        self.storage_door_mqtt_client.connect(host="127.0.0.1", port=1883)
+        self.storage_door_mqtt_client.subscribe(topic="st_72")
+        self.storage_door_mqtt_client.on_message = lambda client, userdata, message: self.assertStorageUsersUpdated(
+            message)
+        self.storage_door_mqtt_client.loop_start()
+        self.storage_door_sentinel = False
 
-        gilberts_card = Card(hash="1234", user="")
-        annas_card = Card(hash="2222")
-        marias_card_1 = Card(hash="2121")
-        marias_card_2 = Card(hash="333")
-        martas_card = Card(hash="4444")
-        mr_misterius_card = Card(hash="4343")
+        self.hall_door_mqtt_client = mqtt.Client()
+        self.hall_door_mqtt_client.connect(host="127.0.0.1", port=1883)
+        self.hall_door_mqtt_client.subscribe(topic="hl-19")
+        self.hall_door_mqtt_client.on_message = lambda client, userdata, message: self.assertHallUsersUpdated(
+            message)
+        self.hall_door_mqtt_client.loop_start()
+        self.hall_door_sentinel = False
 
-        curent_rule_hk.save()
-        curent_rule_s.save()
-        old_rule.save()
+    def assertSecurityUsersUpdated(self, message):
+        data = json.loads(message.payload)
+        self.assertDictEqual(data, security_door_result)
+        self.security_door_sentinel = True
 
-        housekeeping.save()
-        old_residents.save()
-        security.save()
+    def assertStorageUsersUpdated(self, message):
+        data = json.loads(message.payload)
+        self.assertDictEqual(data, storage_door_result)
+        self.storage_door_sentinel = True
 
-        housekeeping.rules.add(curent_rule_hk)
-        housekeeping.save()
-        security.rules.add(curent_rule_s)
-        security.save()
-        old_residents.rules.add(old_rule)
-        old_residents.save()
+    def assertHallUsersUpdated(self, message):
+        data = json.loads(message.payload)
+        self.assertDictEqual(data, hall_door_result)
+        self.hall_door_sentinel = True
 
-        gilbert.save()
-        anna.save()
-        maria.save()
-        marta.save()
-        mr_misterius.save()
-
-        gilbert.groups.add(housekeeping)
-        gilbert.save()
-        anna.groups.add(housekeeping)
-        anna.save()
-        maria.groups.add(security)
-        maria.save()
-        marta.groups.add(security)
-        marta.save()
-        mr_misterius.groups.add(old_residents)
-        mr_misterius_card.save()
-
-        gilberts_card.user = "Gilbert"
-        gilberts_card.save()
-        annas_card.user = "Anna"
-        annas_card.save()
-        marias_card_1.user = "Maria"
-        marias_card_1.save()
-        marias_card_2.user = "Maria"
-        marias_card_2.save()
-        martas_card.user = "Marta"
-        martas_card.save()
-        mr_misterius_card.user = "Mr_Misterius"
-        mr_misterius_card.save()
-
+    def test_update_doors(self):
+        C.get(path=reverse('update_doors'))
+        print("waiting for mqtt publishing ...")
+        time.sleep(3)
+        self.assertTrue(self.security_door_sentinel)
+        self.assertTrue(self.storage_door_sentinel)
+        self.assertTrue(self.hall_door_sentinel)
