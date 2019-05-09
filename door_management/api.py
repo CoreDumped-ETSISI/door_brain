b3ee906db7364c98daef6f0bb185d3a1639448ce
LordMascachapas <alfredm3007@gmail.com>
2019-05-09 12:29:43 +0200

    endpoint to send a message to a single door

--- door_management/api.py
+++ door_management/api.py
@@ -3,12 +3,13 @@ from rest_framework.response import Response
 from paho.mqtt import client as mqtt
 from door_brain.settings import MQTT_SETTINGS, BROKER_DUTIES
 from mqtt_brokers.models import Broker
+from doors.models import Door
 import json
 
 mqtt_manager = mqtt.Client(client_id=MQTT_SETTINGS.get("CLIENT_ID"))
 
 
-class MqttSendMessage(APIView):
+class MqttSendMessageBroadcast(APIView):
     def get(self, request, message):
         brokers = Broker.objects.filter(duty=BROKER_DUTIES.get("MANAGEMENT"))
         error_message = []
@@ -18,7 +19,7 @@ class MqttSendMessage(APIView):
             try:
                 mqtt_manager.connect(host=broker.ip, port=broker.port)
                 mqtt_manager.publish(
-                    topic=MQTT_SETTINGS.get('TOPICS').get('MANAGEMENT'),
+                    topic=MQTT_SETTINGS.get('TOPICS').get('MANAGEMENT_BROADCAST'),
                     payload=message
                 )
             except Exception as err:
@@ -26,6 +27,31 @@ class MqttSendMessage(APIView):
                     'broker_ip': broker.ip,
                     'ERROR': err.args
                 }]
+        return Response({
+            "Message": 'Message sent',
+            "ERRORS": error_message
+        }, status=200)
+
+
+class MqttSendMessageDetail(APIView):
+    def get(self, request, door_id, message):
+        door_set = Door.objects.filter(id=door_id)
+        if len(door_set) is 0:
+            return Response({'ERROR': 'Door not found'}, status=404)
+        door = door_set[0]
+        manage_broker = door.manage_broker
+        error_message = []
+        try:
+            mqtt_manager.connect(host=manage_broker.ip, port=manage_broker.port)
+            mqtt_manager.publish(
+                topic=MQTT_SETTINGS.get('TOPICS').get('MANAGEMENT_BROADCAST'),
+                payload=message
+            )
+        except Exception as err:
+            error_message += [{
+                'broker_ip': manage_broker.ip,
+                'ERROR': err.args
+            }]
         return Response({
             "Message": 'Message sent',
             "ERRORS": error_message
