4cee9d5cc3ace5ce81bd909b2d31610efdc15563
LordMascachapas <alfredm3007@gmail.com>
2019-05-09 13:42:12 +0200

    comment future endpoint

--- door_management/urls.py
+++ door_management/urls.py
@@ -6,5 +6,5 @@ base_url = 'manage/'
 urlpatterns = [
     path(base_url + 'sendMessage/<str:message>', MqttSendMessageBroadcast.as_view(), name="mqtt_send_message_broadcast"),
     path(base_url + 'sendMessage/<str:door_id>/<str:message>', MqttSendMessageDetail.as_view(), name="mqtt_send_message_detail")
-    # path(base_url + "updateUsers", SomeView.as_view())
+    # path(base_url + "updateUsers", SomeView.as_view(), name="update_doors")
 ]
