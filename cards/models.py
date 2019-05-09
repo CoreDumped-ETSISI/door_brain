949c3c19aab3c0d0f393fa4c842468685903afc4
LordMascachapas <alfredm3007@gmail.com>
2019-05-09 09:59:02 +0200

    Cards with initial and expiration date
    Also fix custom user import

--- cards/models.py
+++ cards/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from custom_users.models import CustomUser
+from custom_users.models import User
 
 
 class Card(models.Model):
     hash = models.CharField(primary_key=True, max_length=300, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    initial_date = models.DateField(null=True)
+    expiration_date = models.DateField(null=True)
 
     def __str__(self):
         return self.hash[0:3] + ' ' + self.user.username + ' (created at ' + self.created_at.strftime('%Y-%m-%d') + ')'
