2dc101e8f9bb76341393b2a6b5d9f8eef72b717a
LordMascachapas <alfredm3007@gmail.com>
2019-05-09 10:04:10 +0200

    mqtt_groups with init/exp dates

--- mqtt_groups/models.py
+++ mqtt_groups/models.py
@@ -5,6 +5,8 @@ from mqtt_rules.models import WeekRules
 class MqttGroup(models.Model):
     name = models.CharField(primary_key=True, max_length=100, default=None)
     rules = models.ManyToManyField(WeekRules)
+    initial_date = models.DateField(null=True)
+    expiration_date = models.DateField(null=True)
 
     def __str__(self):
         return self.name
