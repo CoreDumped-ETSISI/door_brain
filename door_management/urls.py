1c975715b42f195a91269f0740744e5e376e64d4
LordMascachapas <alfredm3007@gmail.com>
2019-05-09 12:35:37 +0200

    send detail message path

--- door_management/urls.py
+++ door_management/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from door_management.api import MqttSendMessage
+from door_management.api import MqttSendMessageBroadcast, MqttSendMessageDetail
 
 base_url = 'manage/'
 
 urlpatterns = [
-    path(base_url + 'sendMessage/<str:message>', MqttSendMessage.as_view(), name="mqtt_send_message")
+    path(base_url + 'sendMessage/<str:message>', MqttSendMessageBroadcast.as_view(), name="mqtt_send_message_broadcast"),
+    path(base_url + 'sendMessage/<str:door_id>/<str:message>', MqttSendMessageDetail.as_view(), name="mqtt_send_message_detail")
+    # path(base_url + "updateUsers", SomeView.as_view())
 ]
