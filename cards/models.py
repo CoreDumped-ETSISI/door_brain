a760a193d29af741f72262985c465cd16077a114
LordMascachapas <alfredm3007@gmail.com>
2019-05-10 13:18:53 +0200

    Fixes in Card model
    Expiration date must be greater than initial date

--- cards/models.py
+++ cards/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from custom_users.models import User
+from django.core.exceptions import ValidationError
 
 
 class Card(models.Model):
@@ -8,6 +9,10 @@ class Card(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     initial_date = models.DateField(null=True)
     expiration_date = models.DateField(null=True)
+
+    def clean_fields(self, exclude=None):
+        if self.initial_date >= self.expiration_date:
+            raise ValidationError("Expiration date must be greater than initial date")
 
     def __str__(self):
         return self.hash[0:3] + ' ' + self.user.username + ' (created at ' + self.created_at.strftime('%Y-%m-%d') + ')'
