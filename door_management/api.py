e68088e7bc459c043853c98771c3676171dac0c5
LordMascachapas <alfredm3007@gmail.com>
2019-05-13 19:47:24 +0200

    Instantiate an mqtt client for each request

--- door_management/api.py
+++ door_management/api.py
@@ -6,8 +6,6 @@ from mqtt_brokers.models import Broker
 from doors.models import Door
 import json
 
-mqtt_manager = mqtt.Client(client_id=MQTT_SETTINGS.get("CLIENT_ID"))
-
 
 class MqttSendMessageBroadcast(APIView):
     def get(self, request, message):
@@ -17,6 +15,7 @@ class MqttSendMessageBroadcast(APIView):
             return Response({'ERROR': 'No Manage brokers registered'}, status=404)
         for broker in brokers:
             try:
+                mqtt_manager = mqtt.Client(client_id=MQTT_SETTINGS.get("CLIENT_ID"))
                 mqtt_manager.connect(host=broker.ip, port=broker.port)
                 mqtt_manager.publish(
                     topic=MQTT_SETTINGS.get('TOPICS').get('MANAGEMENT_BROADCAST'),
@@ -38,10 +37,12 @@ class MqttSendMessageDetail(APIView):
         door_set = Door.objects.filter(id=door_id)
         if len(door_set) is 0:
             return Response({'ERROR': 'Door not found'}, status=404)
+
         door = door_set[0]
         manage_broker = door.manage_broker
         error_message = []
         try:
+            mqtt_manager = mqtt.Client(client_id=MQTT_SETTINGS.get("CLIENT_ID"))
             mqtt_manager.connect(host=manage_broker.ip, port=manage_broker.port)
             mqtt_manager.publish(
                 topic=door.manage_topic,
