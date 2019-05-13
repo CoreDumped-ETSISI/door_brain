d8efdc48f0986637a1c56bbaf0cb961811d53834
LordMascachapas <alfredm3007@gmail.com>
2019-05-13 19:48:34 +0200

    Method for MqttGroup model which retrieve the time_table

--- mqtt_groups/models.py
+++ mqtt_groups/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from mqtt_rules.models import WeekRules
 from django.core.exceptions import ValidationError
+from datetime import time
 
 
 class MqttGroup(models.Model):
@@ -15,3 +16,28 @@ class MqttGroup(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_time_table(self):
+        time_table = {
+            'L': [],
+            'M': [],
+            'X': [],
+            'J': [],
+            'V': [],
+            'S': [],
+            'D': []
+        }
+        rules = self.rules.get_queryset()
+        for rule in rules:
+            days = rule.days_per_week
+            for day in days:
+                init_time = rule.initial_hour_access
+                final_hour = (init_time.hour + (rule.duration.seconds // 3600)) % 24
+                final_minute = init_time.minute + ((rule.duration.seconds % 3600) // 60)
+                final_time = time(hour=final_hour, minute=final_minute)
+                time_period = [
+                    init_time.strftime('%H:%M'),
+                    final_time.strftime('%H:%M')
+                ]
+                time_table[day].append(time_period)
+        return time_table
