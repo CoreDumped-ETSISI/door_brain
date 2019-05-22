abca749877cca18025a47ad80589b48c6bc24307
LordMascachapas <alfredm3007@gmail.com>
2019-05-22 13:35:44 +0200

    fixes in mqtt manager

--- door_brain/utils/mqtt.py
+++ door_brain/utils/mqtt.py
@@ -5,7 +5,6 @@ from threading import Lock
 class Mqtt:
     __instance = None
     __active_mqtt_clients = []
-    __sleep_mqtt_clients = []
     lock = Lock()
 
     @staticmethod
@@ -15,10 +14,10 @@ class Mqtt:
         return Mqtt.__instance
 
     def __init__(self):
-        if self.__instance is not None:
+        if Mqtt.__instance is not None:
             raise Exception("The Mqtt manager has been already instantiated")
         else:
-            self.__instance = self
+            Mqtt.__instance = self
 
     def get_client(self):
         self.lock.acquire()
@@ -27,31 +26,14 @@ class Mqtt:
         return client
 
     def _generate_client(self):
-        if self.__sleep_mqtt_clients:
-            new_client = self.__sleep_mqtt_clients[0]
-            self.__sleep_mqtt_clients.pop(0)
-        else:
-            new_client = mqtt.Client()
-            self.__active_mqtt_clients.append(new_client)
+        new_client = mqtt.Client()
+        self.__active_mqtt_clients.append(new_client)
         return new_client
 
-    def lull_client(self, client):
+    def del_clients(self):
         self.lock.acquire()
-        if client not in self.__active_mqtt_clients:
-            raise Exception("The client is not active")
-        else:
+        for client in self.__active_mqtt_clients:
+            client.disconnect()
             self.__active_mqtt_clients.remove(client)
-            client.reinitialise()
-            self.__sleep_mqtt_clients.append(client)
+            del client
         self.lock.release()
-
-    def lull_clients(self):
-        self.lock.acquire()
-        for client in self.__sleep_mqtt_clients:
-            self.__active_mqtt_clients.remove(client)
-            client.reinitialise()
-            self.__sleep_mqtt_clients.append(client)
-        self.lock.release()
-
-
-Mqtt()
